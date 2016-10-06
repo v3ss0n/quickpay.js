1d62b36b5264de1167eaa9961a4359875bb57368
Jon Randy <jonathan@paysbuy.com>
2016-10-06 17:11:42 +0700

    Initial refactoring

--- build/checkout.js
+++ build/checkout.js
@@ -4,6 +4,7 @@
 
 
 	var
+
 		parentDomain = '',
 
 		QP_CLOSEMESSAGE = 'close',
@@ -13,146 +14,199 @@
 		REGEXP_CVV = /^[0-9]{3,4}$/g,
 		REGEXP_EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 
-		btnPay = $('.qp-btnpay'),
-		btnClose = $('.qp-btnclose'),
-		heading = $('.qp-title'),
-		image = $('.qp-img'),
-		cover = $('.qp-cover'),
-		desc = $('.qp-desc'),
-		indicator = $('.qp-loading'),
-
-		fld_email = $('#email'),
-		fld_cardNum = $('#card_number'),
-		fld_expiry = $('#card_expiry_date'),
-		fld_cvv = $('#card_cvn'),
-
-		pbKey = '',
-
-		validations = [
-			[ fld_email, [notEmpty, matches(REGEXP_EMAIL)] ],
-			[ fld_cardNum, notEmpty ],
-			[ function() { return fld_cardNum.validateCreditCard(); }, cardValid, fld_cardNum ],
-			[ fld_expiry, [notEmpty, matches(REGEXP_EXPIRY)] ],
-			[ fld_cvv, [notEmpty, matches(REGEXP_CVV)] ]
-		]
+		// predicate functions and predicate function creators
+		_ = {
+			matches: function(regex) { return function(str) { return str.search(regex) !== -1; }; },
+			notEmpty: function(str) { return $.trim(str) !== ''; },
+			cardValid: function(detail) { return detail.valid && detail.length_valid && detail.luhn_valid; }
+		}
 
 	;
 
+
+
+
+	var ui = (function($) {
+
+		var
+			btnPay = $('.qp-btnpay'),
+			btnClose = $('.qp-btnclose'),
+			heading = $('.qp-title'),
+			image = $('.qp-img'),
+			cover = $('.qp-cover'),
+			desc = $('.qp-desc'),
+			indicator = $('.qp-loading'),
+			form = $('form'),
+
+			fld_email = $('#email'),
+			fld_cardNum = $('#card_number'),
+			fld_expiry = $('#card_expiry_date'),
+			fld_cvv = $('#card_cvn'),
+
+			formValidations = [
+				[ fld_email, [_.notEmpty, _.matches(REGEXP_EMAIL)] ],
+				[ fld_cardNum, _.notEmpty ],
+				[ function() { return fld_cardNum.validateCreditCard(); }, _.cardValid, fld_cardNum ],
+				[ fld_expiry, [_.notEmpty, _.matches(REGEXP_EXPIRY)] ],
+				[ fld_cvv, [_.notEmpty, _.matches(REGEXP_CVV)] ]
+			]
+
+		;
+
+		function init() {
+			btnPay.on('click', pay);
+			cover.add(btnClose).on('click', closeCheckout);
+			$(document).on('keyup', _closeOnEsc);
+			fld_cardNum.validateCreditCard(_setCardTypeOnField);
+		}
+
+		function _setCardTypeOnField(card) {
+			$(this).attr('data-cardtype', card.card_type && card.card_type.name);
+		}
+
+		function showInvalidFields(fields) {
+			alert('invalid');
+			// todo - highlight fields here
+		}
+
+		function showProcessIndicator(state) {
+			state = !(state === false);
+			indicator.toggle(state);
+			btnPay.prop('disabled', state);
+		}
+
+		function setupForm(d) {
+			_clearFields();
+			// heading
+			heading.html(d.title).toggle(!!d.title);
+			// image
+			image.attr({src:d.image, alt:d.title}).toggle(!!d.image);
+			// description
+			desc.html(d.description).toggle(!!d.description);
+			// button text
+			btnPay.val(getButtonLabel(d.submitLabel, d.amount, d.currency));
+			// focus first field
+			$('form input').eq(0).focus();
+		}
+
+		function _clearFields() {
+			$('form input[type=text]').val('');
+		}
+
+		function _closeOnEsc(e) {
+			if (e.keyCode == KEYCODE_ESC) closeCheckout(); 
+		}
+
+		function getFormData() {
+			var
+				formdata = form.serializeArray(),
+				data = {}
+			;
+			$(formdata).each(function(i, obj){
+				data[obj.name] = obj.value;
+			});
+			return data;
+		}
+
+		return {
+			init: init,
+			showInvalidFields: showInvalidFields,
+			showProcessIndicator: showProcessIndicator,
+			form: form,
+			setupForm: setupForm,
+			getFormData: getFormData,
+			formValidations: formValidations
+		}
+
+	})($);
+
+
+
+
+	var validator = (function($) {
+
+		function run(checks) {
+			// run validation check(s) - return true if all passing, or an array of errored fields if not
+			var res, errs = [];
+			$.each(checks, function(i, check) {
+				res = _checkField.apply(0, check);
+				if (res !== true) errs.push(res);
+			});
+			return errs.length ? errs : true;
+		}
+
+		function _checkField(valueOrGetter, checksToRun, relatedField) {
+			// check a single value with given checks
+			var
+				valueIsJQ = valueOrGetter instanceof $,
+				toCheck = valueIsJQ ? valueOrGetter.val() : (typeof valueOrGetter == 'function') ? valueOrGetter() : valueOrGetter,
+				validatorList = (checksToRun instanceof Array) ? checksToRun : [ checksToRun ],
+				field = relatedField || (valueIsJQ ? valueOrGetter : false),
+				res = false
+			;
+			$.each(validatorList, function(i, validateFunc) { return res = validateFunc(toCheck); });
+			return res || field;
+		}
+
+		return {
+			run: run 
+		};
+
+	})($);
+
+
+	var tokenizer = (function() {
+
+		function process(data, callback) {
+			// make sure Expiry date is in correct format if it's there
+			if (data.card_expiry_date) data.card_expiry_date = _sanitiseExpiry(data.card_expiry_date);
+			Paysbuy.setPublicKey(tokenizer.key);
+			Paysbuy.createToken(data, callback);
+		}
+
+		function _sanitiseExpiry(dt) {
+			var
+				matches = REGEXP_EXPIRY.exec(dt),
+				mm = matches[1],
+				yyyy = matches[2]
+			;
+			if (yyyy.length==2) yyyy = '20'+yyyy;
+			REGEXP_EXPIRY.lastIndex = 0; // reset regex - IMPORTANT
+			return mm+'-'+yyyy;
+		}
+
+		return {
+			process: process
+		};
+
+	})();
+
+
 	function init() {
 		listenToParent();
-		btnPay.on('click', pay);
-		cover.add(btnClose).on('click', closeCheckout);
-		$(document).on('keyup', closeOnEsc);
-		fld_cardNum.validateCreditCard(setCardTypeOnField);
+		ui.init();
 	}
 
-	function setCardTypeOnField(card) {
-		$(this).attr('data-cardtype', card.card_type && card.card_type.name);
-	}
 
 	function pay() {
-		var valid = validate(validations);
+		var valid = validator.run(ui.formValidations);
 		if (valid === true) {
-			showIndicator();
-			doTokenization(getFormData(true), _tokenizationDone);
+			ui.showProcessIndicator();
+			tokenizer.process(ui.getFormData(true), receiveTokenizationResult);
 		} else {
-			showInvalidFields(valid)
+			ui.showInvalidFields(valid);
 		}
 	}
 
-	function showInvalidFields(fields) {
-		console.log(fields);
-		alert('invalid');
-		// todo - highlight fields here
-	}
-
-	function validate(checks) {
-		// run validation check(s) - return true if all passing, or an array of errored fields if not
-		var res, errs = [];
-		$.each(checks, function(i, check) {
-			res = checkField.apply(0, check);
-			if (res !== true) errs.push(res);
-		});
-		return errs.length ? errs : true;
-	}
-
-	function checkField(valueOrGetter, validators, relatedField) {
-		// check a single value with given validators
-		var
-			valueIsJQ = valueOrGetter instanceof $,
-			toCheck = valueIsJQ ? valueOrGetter.val() : (typeof valueOrGetter == 'function') ? valueOrGetter() : valueOrGetter,
-			validatorList = (validators instanceof Array) ? validators : [ validators ],
-			field = relatedField || (valueIsJQ ? valueOrGetter : false),
-			res = false
-		;
-		$.each(validatorList, function(i, validateFunc) { return res = validateFunc(toCheck); });
-		return res || field;
-	}
-
-	function matches(regexp) {
-		return function(str) {
-			return str.search(regexp) !== -1;
-		};
-	}
-
-	function notEmpty(str) {
-		return $.trim(str) != '';
-	}
-
-	function cardValid(detail) {
-		// check passed card detail object (from the creditCardValidator jquery ext) is valid
-		return detail.valid && detail.length_valid && detail.luhn_valid;
-	}
-
-	function showIndicator(state) {
-		state = !(state === false);
-		indicator.toggle(state);
-		btnPay.prop('disabled', state);
-	}
-
-	function getFormData(includeCard) {
-		var
-			formdata = $("form").serializeArray(),
-			data = {}
-		;
-		$(formdata).each(function(i, obj){
-			data[obj.name] = obj.value;
-		});
-
-		if (!includeCard) {
-			delete data.card_number;
-			delete data.card_expiry_date;
-			delete data.card_cvn;
-		}
-
-		// make sure Expiry date is in correct format if it's there
-		if (data.card_expiry_date) data.card_expiry_date = sanitiseExpiry(data.card_expiry_date);
-
-		data.key = pbKey;
-		return data;
-	}
-
-	function sanitiseExpiry(dt) {
-		console.log('REGEXP is '+REGEXP_EXPIRY);
-		console.log('Date is '+dt);
-		var
-			matches = REGEXP_EXPIRY.exec(dt),
-			mm = matches[1],
-			yyyy = matches[2]
-		;
-		if (yyyy.length==2) yyyy = '20'+yyyy;
-		REGEXP_EXPIRY.lastIndex = 0; // reset regex - IMPORTANT
-		return mm+'-'+yyyy;
-	}
 
 	function returnSuccess(token) {
-		var details = getFormData();
+		var details = ui.getFormData();
 		details.token = token;
 		messageParent(JSON.stringify(details));
 	}
 
-	function _tokenizationDone(status, response) {
-		showIndicator(false);
+	function receiveTokenizationResult(status, response) {
+		ui.showProcessIndicator(false);
 		if (response.token) {
 			returnSuccess(response.token);
 		} else {
@@ -160,18 +214,10 @@
 		}
 	}
 
-	function doTokenization(data, callback) {
-		Paysbuy.setPublicKey(data.key);
-		Paysbuy.createToken(data, callback);
-	}
-
 	function closeCheckout() {
 		messageParent(QP_CLOSEMESSAGE);
 	}
 
-	function closeOnEsc(e) {
-		if (e.keyCode == KEYCODE_ESC) closeCheckout(); 
-	}
 
 	function messageParent(msg) {
 		parent.window.postMessage(msg, parentDomain);
@@ -182,24 +228,7 @@
 		window.addEventListener("message", parentListener, false);
 	}
 
-	function setupForm(d) {
-		pbKey = d.key;
-		clearFields();
-		// heading
-		heading.html(d.title).toggle(!!d.title);
-		// image
-		image.attr({src:d.image, alt:d.title}).toggle(!!d.image);
-		// description
-		desc.html(d.description).toggle(!!d.description);
-		// button text
-		btnPay.val(getButtonLabel(d.submitLabel, d.amount, d.currency));
-		// focus first field
-		$('form input').eq(0).focus();
-	}
 
-	function clearFields() {
-		$('form input[type=text]').val('');
-	}
 
 	function getButtonLabel(txt, amount, curr) {
 		return txt.replace('{amount}', formatAmount(amount, curr));
@@ -211,9 +240,14 @@
 	}
 
 	function parentListener(ev) {
+		var d;
 		if (ev.data) {
 			if (!parentDomain) parentDomain = ev.origin;
-			if (parentDomain == ev.origin) setupForm(JSON.parse(ev.data));
+			if (parentDomain == ev.origin) {
+				d = JSON.parse(ev.data);
+				tokenizer.key = d.key;
+				ui.setupForm(d);
+			}
 		}
 	}
 
